179459a3105515a2eb0221fbdc7e79c30709e780
shreya suman <shreyasumanck@gmail.com>
2019-05-18 21:42:09 -0400

    Initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,41 @@
+var friends = [
+    {
+        name: "Rihanna Fenty",
+        photo: "https://en.wikipedia.org/wiki/Rihanna#/media/File:Rihanna_Fenty_2018.png",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    }
+},
+{
+    name: "Blake Lively",
+    photo: "https://timedotcom.files.wordpress.com/2014/07/491456305.jpg",
+    scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+},
+{
+    name: "Drake Graham",
+    photo: "https://www.discogs.com/artist/151199-Drake",
+    scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+},
+{
+    name: "Chris Brown",
+    photo: "https://www.google.com/search?q=chris+brown&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj_y5iA_qPiAhUQhOAKHZ_0AgwQ_AUIDygC&biw=1132&bih=727#imgrc=27B0tIgA28X9wM:"
+    scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+},
+{
+    name: "Kim Kardashian",
+    photo: "https://www.google.com/search?q=kim+kardashian&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjt-Z3JjaTiAhXimuAKHakRBiEQ_AUIDygC&biw=1132&bih=727#imgrc=JIgIKDMu9pfoGM:"
+    scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+},
+{
+    name: "Khloe Kardashian"
+    photo: "https://www.google.com/search?biw=1132&bih=727&tbm=isch&sa=1&ei=BXjfXLOGLIeQ_QaE6pjoAw&q=khloe+kardashian&oq=khloe+kardashian&gs_l=img.1.0.0i67l2j0l3j0i67j0l4.2024712.2026739..2028455...0.0..0.74.378.6......1....1..gws-wiz-img.......0i7i30.Z1nZELTAgTc#imgrc=F07jpK8GnmDEjM:"
+    scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+},
+{
+    name: "Margot Robbie"
+    photo: "https://www.google.com/search?biw=1132&bih=727&tbm=isch&sa=1&ei=8n_fXOibMI-o_Qa-ybagBQ&q=margot+robbie&oq=Margot&gs_l=img.1.0.0i67j0l2j0i67j0j0i67j0l4.248606.258024..259529...0.0..0.80.1367.21......2....1..gws-wiz-img.....0..35i39.I6N1NsCtjdE#imgrc=WQ9RYmKulb3-fM:"
+    scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+}
+};
+
+module.exports =friends;
+
